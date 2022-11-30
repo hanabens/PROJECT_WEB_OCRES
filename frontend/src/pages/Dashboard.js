b0401d913ffb9ce63841527241ac139b9bb57fdf
hanabens <94543855+hanabens@users.noreply.github.com>
2022-11-30 15:46:27 +0100

    v8

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -2,12 +2,15 @@ import Horloge from "../components/horloge";
 import Note from "../components/Note";
 import Spotify from "../components/Spotify/spotify";
 
+
+
 const Dashboard = () => {
   return (
     <div>
       <Horloge />
       <Note />
       <Spotify />
+
     </div>
   );
 };
