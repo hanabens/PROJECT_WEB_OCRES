48440d104d470390ee509fcdde093e69a93a870a
hanabens <94543855+hanabens@users.noreply.github.com>
2022-12-05 13:39:43 +0100

    fix: Correction buttons

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -4,16 +4,19 @@ import Spotify from "../components/Spotify/spotify";
 import Plot from "../components/Charts/plot";
 import image from "./image.png";
 import Meteo from "../components/Meteo/meteo";
+import { Link } from "react-router-dom"
 
 
 const Dashboard = () => {
   return (
     <div>
-      <img className="image" src={image} /> <Note />
+      <Link to={"/"}>
+        <img className="image" src={image} />
+      </Link>
+      <Note />
       <Spotify />
       <Horloge />
       <Plot />
-      
       <Meteo />
     </div>
   );
